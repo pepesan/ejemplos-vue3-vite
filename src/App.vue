438efc85944b7269db6583fb798776a18a727566
David Vaquero <pepesan@gmail.com>
2026-04-08 19:19:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import PrimerComponente from "./components/PrimerComponente.vue";
+</script>
+
+<template>
+  <PrimerComponente/>
+</template>
---
+++ src/components/PrimerComponente.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <button @click="incrementar">
+    Contador: {{ contador }}
+  </button>
+</template>
+
+<style scoped>
+
+</style>
