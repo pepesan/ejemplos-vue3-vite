d73034cc1a658a133e5276955d2a7936fbe6c400
David Vaquero <pepesan@gmail.com>
2026-04-12 12:23:59 +0200

    metidos ejemplos de pinia

--- src/modules/profile/pages/ProfileSettingsPage.vue
+++ src/modules/profile/pages/ProfileSettingsPage.vue
@@ -1,9 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useProfileStore } from '../stores/profileStore'
+
+const profileStore = useProfileStore()
+const email = ref(profileStore.contactEmail)
+
+const saveEmail = () => {
+  profileStore.updateEmail(email.value)
+}
+</script>
+
 <template>
   <section>
     <h2>Configuración del perfil</h2>
-    <p>Aquí el usuario puede modificar sus datos.</p>
+
+    <label for="email">Email de contacto:</label>
+    <input
+        id="email"
+        v-model="email"
+        type="email"
+    />
+
+    <button @click="saveEmail">
+      Guardar cambios
+    </button>
+
+    <p>Email actual: {{ profileStore.contactEmail }}</p>
   </section>
 </template>
-
-<script setup lang="ts">
-</script>
