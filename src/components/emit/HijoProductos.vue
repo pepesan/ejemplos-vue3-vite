a5d526bb7a39828133c5b60f5d53897165fa92e8
David Vaquero <pepesan@gmail.com>
2026-04-14 16:00:33 +0200

    mejorados ejemplos de emit

---
+++ src/components/emit/HijoProductos.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'agregarProducto', nombre: string, precio: number): void
+  (e: 'mostrarMensaje'): void
+}>()
+
+function agregar() {
+  emit('agregarProducto', 'Portátil', 899)
+}
+
+function mostrar() {
+  emit('mostrarMensaje')
+}
+</script>
+
+<template>
+  <div>
+    <button @click="agregar">
+      Agregar producto
+    </button>
+
+    <button @click="mostrar">
+      Mostrar mensaje
+    </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/emit/PadreProductos.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HijoProductos from './HijoProductos.vue'
+
+const nombreProducto = ref('')
+const precioProducto = ref(0)
+const mensaje = ref('')
+
+function manejarAgregarProducto(nombre: string, precio: number) {
+  nombreProducto.value = nombre
+  precioProducto.value = precio
+}
+
+function manejarMensaje() {
+  mensaje.value = 'Evento sin parámetros recibido'
+}
+</script>
+
+<template>
+  <h2>Ejemplo con parámetros</h2>
+
+  <HijoProductos
+      @agregarProducto="manejarAgregarProducto"
+      @mostrarMensaje="manejarMensaje"
+  />
+
+  <p v-if="nombreProducto">
+    Producto: {{ nombreProducto }}
+  </p>
+
+  <p v-if="precioProducto">
+    Precio: {{ precioProducto }} €
+  </p>
+
+  <p v-if="mensaje">
+    {{ mensaje }}
+  </p>
+</template>
+
+<style scoped>
+</style>
