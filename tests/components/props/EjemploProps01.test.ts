d75c3e3008aec513e7eb18821e255cbcbe1a730b
David Vaquero <pepesan@gmail.com>
2026-04-22 16:00:28 +0200

    Mejorado el fichero de config de vitest

--- tests/components/props/EjemploProps01.test.ts
+++ tests/components/props/EjemploProps01.test.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest'
-import EjemploProps01 from '../../../src/components/props/EjemploProps01.vue'
+import {describe, it, expect, test} from 'vitest'
+import EjemploProps01 from '@/components/props/EjemploProps01.vue'
 
 describe('EjemploProps01', () => {
     it('muestra el nombre recibido por props', () => {
@@ -11,5 +11,27 @@ describe('EjemploProps01', () => {
         })
 
         expect(wrapper.text()).toContain('Hola Juan')
+    })
+    it('muestra otro nombre recibido por props', () => {
+        const wrapper = mount(EjemploProps01, {
+            props: {
+                nombre: 'Pepe'
+            }
+        })
+
+        expect(wrapper.text()).toContain('Hola Pepe')
+    })
+    // Prueba parametrizada
+    test.each([
+        ["Juan", "Hola Juan"],
+        ["Pepe", "Hola Pepe"],
+
+    ])('Prop(%s) Expected (%s)', (prop: string, expected:string) => {
+        const wrapper = mount(EjemploProps01, {
+            props: {
+                nombre: prop
+            }
+        })
+        expect(wrapper.text()).toContain(expected);
     })
 })
